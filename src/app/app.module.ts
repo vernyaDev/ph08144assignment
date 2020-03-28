dc69382d43c03436cd94165f0ff3c54d4aa1bc9a
vernyaDev <tllongvl3@gmail.com>
2020-03-29 00:54:26 +0700

    2 files modified, 12 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SliderComponent } from './slider/slider.component';
+import { HomeProductComponent } from './home-product/home-product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, FooterComponent, HeaderComponent ],
+  declarations: [ AppComponent, HelloComponent, FooterComponent, HeaderComponent, HomeComponent, SliderComponent, HomeProductComponent, ProductListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
